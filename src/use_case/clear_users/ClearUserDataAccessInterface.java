cc8c586740eebd2365eabd7131aa65eabc0111bc
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-11-19 15:48:15 -0500

    Finished, added get users and clear

--- src/use_case/clear_users/ClearUserDataAccessInterface.java
+++ src/use_case/clear_users/ClearUserDataAccessInterface.java
@@ -2,5 +2,14 @@ package use_case.clear_users;
 
 // TODO Complete me
 
+import entity.User;
+
 public interface ClearUserDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    void save(User user);
+
+    public String getUsers();
+
+    public void clear();
 }
