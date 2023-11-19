724d43f3a264182b4eab53213ae23238ea09ad69
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-11-19 15:46:33 -0500

    ClearInteractor finished

--- src/use_case/clear_users/ClearInteractor.java
+++ src/use_case/clear_users/ClearInteractor.java
@@ -1,6 +1,26 @@
 package use_case.clear_users;
+import data_access.FileUserDataAccessObject;
+import entity.User;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginUserDataAccessInterface;
 
+import java.util.List;
 // TODO Complete me
 
-public class ClearInteractor {
+public class ClearInteractor implements ClearInputBoundary {
+    @Override
+    public void execute(ClearInputData clearInputData) {
+        clearUserDAO.getUsers();
+        clearUserDAO.clear();
+    }
+
+    final ClearUserDataAccessInterface clearUserDAO;
+    final ClearOutputBoundary clearPresenter;
+
+    public ClearInteractor(ClearUserDataAccessInterface clearUserDAO,
+                           ClearOutputBoundary clearPresenter) {
+        this.clearUserDAO = clearUserDAO;
+        this.clearPresenter = clearPresenter;
+    }
+
 }
