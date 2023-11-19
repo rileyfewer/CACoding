eefcf5fd0be8ec8699c37a2aaab018a4f7fc1265
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-11-19 15:49:18 -0500

    initial commit

--- src/interface_adapter/clear_users/ClearState.java
+++ src/interface_adapter/clear_users/ClearState.java
@@ -2,5 +2,20 @@ package interface_adapter.clear_users;
 
 // TODO Complete me
 
+import interface_adapter.login.LoginState;
+
 public class ClearState {
+    private String username = "";
+
+    public ClearState(ClearState copy) {
+        username = copy.username;
+    }
+    public ClearState() {}
+
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
